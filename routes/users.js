11cee123434a7727fe04cc24b00917efe842d997
Maria P <misterrian@yandex.ru>
2022-08-21 23:37:58 +0300

    Project 13: Tests fix #4

--- routes/users.js
+++ routes/users.js
@@ -12,5 +12,5 @@ module.exports = express.Router()
   .get('/', getAllUsers)
   .get('/:userId', getUserById)
   .post('/', addUser)
-  .post('/me', updateProfile)
-  .post('/me/avatar', updateAvatar);
+  .patch('/me', updateProfile)
+  .patch('/me/avatar', updateAvatar);
