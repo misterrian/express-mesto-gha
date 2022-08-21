db8df1b602b426cebe3ce0d041f637d97824a294
Maria P <misterrian@yandex.ru>
2022-08-21 23:32:29 +0300

    Project 13: Tests fix #3

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,5 @@
 const { DocumentNotFoundError, ValidationError } = require('mongoose').Error;
+const ObjectId = require('mongoose').Types.ObjectId;
 const User = require('../models/user');
 const { sendMessage } = require('../utils/utils');
 
@@ -9,7 +10,14 @@ const getAllUsers = (req, res) => {
 };
 
 const getUserById = (req, res) => {
-  User.findById(req.params.userId)
+  const userId = req.params.userId;
+
+  if (!ObjectId.isValid(userId)) {
+    sendMessage(res, 400, 'Не корректный id пользователя');
+    return;
+  }
+
+  User.findById(userId)
     .orFail()
     .then((user) => res.send(user))
     .catch((err) => {
@@ -39,47 +47,33 @@ const addUser = (req, res) => {
 const updateProfile = (req, res) => {
   const { name, about } = req.body;
 
-  if (Object.keys(req.body).length > 2) {
-    sendMessage(res, 400, 'Переданы лишние параметры');
-  } else if (!name) {
-    sendMessage(res, 400, 'Не указано имя пользователя');
-  } else if (!about) {
-    sendMessage(res, 400, 'Не указано описание пользователя');
-  } else {
-    User.findByIdAndUpdate(req.user._id, { name, about }, { new: true })
-      .orFail()
-      .then((user) => res.send(user))
-      .catch((err) => {
-        if (err instanceof ValidationError) {
-          sendMessage(res, 400, 'Переданы некорректные данные');
-        } else if (err instanceof DocumentNotFoundError) {
-          sendMessage(res, 404, 'Запрашиваемый пользователь не найден');
-        } else {
-          sendMessage(res, 500, 'Произошла ошибка');
-        }
-      });
-  }
+  User.findByIdAndUpdate(req.user._id, { name, about }, { new: true })
+    .orFail()
+    .then((user) => res.send(user))
+    .catch((err) => {
+      if (err instanceof ValidationError) {
+        sendMessage(res, 400, 'Переданы некорректные данные');
+      } else if (err instanceof DocumentNotFoundError) {
+        sendMessage(res, 404, 'Запрашиваемый пользователь не найден');
+      } else {
+        sendMessage(res, 500, 'Произошла ошибка');
+      }
+    });
 };
 
 const updateAvatar = (req, res) => {
   const { avatar } = req.body;
 
-  if (Object.keys(req.body).length > 1) {
-    sendMessage(res, 400, 'Переданы лишние параметры');
-  } else if (!avatar) {
-    sendMessage(res, 400, 'Не указан аватар пользователя');
-  } else {
-    User.findByIdAndUpdate(req.user._id, { avatar }, { new: true })
-      .orFail()
-      .then((user) => res.send(user))
-      .catch((err) => {
-        if (err instanceof DocumentNotFoundError) {
-          sendMessage(res, 404, 'Запрашиваемый пользователь не найден');
-        } else {
-          sendMessage(res, 500, 'Произошла ошибка');
-        }
-      });
-  }
+  User.findByIdAndUpdate(req.user._id, { avatar }, { new: true })
+    .orFail()
+    .then((user) => res.send(user))
+    .catch((err) => {
+      if (err instanceof DocumentNotFoundError) {
+        sendMessage(res, 404, 'Запрашиваемый пользователь не найден');
+      } else {
+        sendMessage(res, 500, 'Произошла ошибка');
+      }
+    });
 };
 
 module.exports = {
